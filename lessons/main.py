a06850b310025b7834490a8ad55c05eea177cdce
BeeDev Classroom <38484069+beedev24@users.noreply.github.com>
2023-01-11 20:53:25 +0000

    updated files and folders

---
+++ lessons/main.py
@@ -0,0 +1,10 @@
+# Basic Data Types
+aString = 'This is a string'
+aInteger = 1
+
+# Other Data or Sequence Types
+aTuple = ('This', 'is', 'a', 'tuple', 'I', 'can', 'not', 'be', 'changed')
+aList = ['I am', 'a list', 'you can', 'add or remove', 'from me']
+aDictionary = {'word': 'definition'}
+
+# Print all of the above
