55cc59a8c4a3d1d1d3db63267121a5ca5ae54461
BeeDev Classroom <38484069+beedev24@users.noreply.github.com>
2023-01-11 21:06:42 +0000

    updated layout

--- lessons/main.py
+++ lessons/main.py
@@ -1,10 +1,12 @@
-# Basic Data Types
-aString = 'This is a string'
-aInteger = 1
-
-# Other Data or Sequence Types
-aTuple = ('This', 'is', 'a', 'tuple', 'I', 'can', 'not', 'be', 'changed')
-aList = ['I am', 'a list', 'you can', 'add or remove', 'from me']
-aDictionary = {'word': 'definition'}
-
-# Print all of the above
+def lessons():
+  # Basic Data Types
+  aString = 'This is a string'
+  aInteger = 1
+  
+  # Other Data or Sequence Types
+  aTuple = ('This', 'is', 'a', 'tuple', 'I', 'can', 'not', 'be', 'changed')
+  aList = ['I am', 'a list', 'you can', 'add or remove', 'from me']
+  aDictionary = {'word': 'definition'}
+  
+  # Print all of the above
+  print(aString)
---
+++ main.py
@@ -0,0 +1,9 @@
+from lessons.main import *
+
+def main():
+  print('Hi')
+  # lessons()
+
+
+if __name__ == "__main__":
+    main()
